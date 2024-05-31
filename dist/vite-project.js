e197da4e0094e25701d010ea3784d5d4cfcec987
pa-lem <pa-lem@hotmail.fr>
2024-05-31 10:40:09 +0200

    remove import + update config

--- dist/vite-project.js
+++ dist/vite-project.js
@@ -1,5 +1,2 @@
-import { jsx as e } from "react/jsx-runtime";
 const t = () => /* @__PURE__ */ e("div", { children: "Remote component here" });
-export {
-  t as RemoteComponent
-};
+export { t as RemoteComponent };
