ddf97421887f423f0d4d3394e6d04c907f0bd19f
pa-lem <pa-lem@hotmail.fr>
2024-06-10 09:12:31 +0200

    update dist files for object storage

--- dist/assets/RemoteComponent-Dv6Ekhww.js
+++ dist/assets/RemoteComponent-Dv6Ekhww.js
@@ -1,6 +1,6 @@
-import { r as reactExports } from './__federation_shared_react-DYlhdcjt.js';
+import { r as reactExports } from "./federation_shared_react-DYlhdcjt.js";
 
-var jsxRuntime = {exports: {}};
+var jsxRuntime = { exports: {} };
 
 var reactJsxRuntime_production_min = {};
 
@@ -13,8 +13,28 @@ var reactJsxRuntime_production_min = {};
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-var f=reactExports,k=Symbol.for("react.element"),l=Symbol.for("react.fragment"),m=Object.prototype.hasOwnProperty,n=f.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,p={key:!0,ref:!0,__self:!0,__source:!0};
-function q(c,a,g){var b,d={},e=null,h=null;void 0!==g&&(e=""+g);void 0!==a.key&&(e=""+a.key);void 0!==a.ref&&(h=a.ref);for(b in a)m.call(a,b)&&!p.hasOwnProperty(b)&&(d[b]=a[b]);if(c&&c.defaultProps)for(b in a=c.defaultProps,a)void 0===d[b]&&(d[b]=a[b]);return {$$typeof:k,type:c,key:e,ref:h,props:d,_owner:n.current}}reactJsxRuntime_production_min.Fragment=l;reactJsxRuntime_production_min.jsx=q;reactJsxRuntime_production_min.jsxs=q;
+var f = reactExports,
+  k = Symbol.for("react.element"),
+  l = Symbol.for("react.fragment"),
+  m = Object.prototype.hasOwnProperty,
+  n = f.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,
+  p = { key: !0, ref: !0, __self: !0, __source: !0 };
+function q(c, a, g) {
+  var b,
+    d = {},
+    e = null,
+    h = null;
+  void 0 !== g && (e = "" + g);
+  void 0 !== a.key && (e = "" + a.key);
+  void 0 !== a.ref && (h = a.ref);
+  for (b in a) m.call(a, b) && !p.hasOwnProperty(b) && (d[b] = a[b]);
+  if (c && c.defaultProps)
+    for (b in ((a = c.defaultProps), a)) void 0 === d[b] && (d[b] = a[b]);
+  return { $$typeof: k, type: c, key: e, ref: h, props: d, _owner: n.current };
+}
+reactJsxRuntime_production_min.Fragment = l;
+reactJsxRuntime_production_min.jsx = q;
+reactJsxRuntime_production_min.jsxs = q;
 
 {
   jsxRuntime.exports = reactJsxRuntime_production_min;
@@ -23,7 +43,9 @@ function q(c,a,g){var b,d={},e=null,h=null;void 0!==g&&(e=""+g);void 0!==a.key&&
 var jsxRuntimeExports = jsxRuntime.exports;
 
 const RemoteComponent = () => {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { children: "Remote component here" });
+  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", {
+    children: "Remote component here",
+  });
 };
 
 export { RemoteComponent as R, jsxRuntimeExports as j };
--- dist/assets/index-DWXMKzUw.js
+++ dist/assets/index-DWXMKzUw.js
@@ -1,6 +1,9 @@
-import { importShared } from './__federation_fn_import-iRn10MJH.js';
-import { j as jsxRuntimeExports, R as RemoteComponent } from './RemoteComponent-Dv6Ekhww.js';
-import { r as reactDomExports } from './__federation_shared_react-dom-81eQxVv-.js';
+import { importShared } from "./federation_fn_import-iRn10MJH.js";
+import {
+  j as jsxRuntimeExports,
+  R as RemoteComponent,
+} from "./RemoteComponent-Dv6Ekhww.js";
+import { r as reactDomExports } from "./federation_shared_react-dom-81eQxVv-.js";
 
 var client = {};
 
@@ -11,10 +14,16 @@ var m = reactDomExports;
 }
 
 function App() {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(RemoteComponent, {}) });
+  return /* @__PURE__ */ jsxRuntimeExports.jsx(jsxRuntimeExports.Fragment, {
+    children: /* @__PURE__ */ jsxRuntimeExports.jsx(RemoteComponent, {}),
+  });
 }
 
-const React = await importShared('react');
-client.createRoot(document.getElementById("root")).render(
-  /* @__PURE__ */ jsxRuntimeExports.jsx(React.StrictMode, { children: /* @__PURE__ */ jsxRuntimeExports.jsx(App, {}) })
-);
+const React = await importShared("react");
+client
+  .createRoot(document.getElementById("root"))
+  .render(
+    /* @__PURE__ */ jsxRuntimeExports.jsx(React.StrictMode, {
+      children: /* @__PURE__ */ jsxRuntimeExports.jsx(App, {}),
+    })
+  );
